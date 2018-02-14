d9a96a220df2f75fdb46e98c4e1ed0610e04b153
Nicole Bekkers <nicole@kabisa.nl>
2018-02-14 13:40:27 +0100

    REFACTOR: combine the two tests
    They test the same functionality

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -2,11 +2,8 @@ import { expect } from "chai";
 import { add } from "../src/calculator";
 
 describe("Calculator", () => {
-  it("adds 1 and 2 together", () => {
+  it("adds two numbers", () => {
     expect(add(1, 2)).to.eql(3)
-  });
-
-  it("adds 2 and 3 together", () => {
     expect(add(2, 3)).to.eql(5)
   });
 });
