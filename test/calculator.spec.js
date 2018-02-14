c130e6219c35180cf4fdf6e40b1a498c21f019fe
Nicole Bekkers <nicole@kabisa.nl>
2018-02-14 11:16:33 +0100

    REFACTOR: Remove first test
    The second test also proves that our environment is set up correctly

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -2,10 +2,6 @@ import { expect } from "chai";
 import { add } from "../src/calculator";
 
 describe("Calculator", () => {
-  it("runs the test and shows the result", () => {
-    expect(true).to.eql(true)
-  });
-
   it("calls the add function", () => {
     add();
   });
