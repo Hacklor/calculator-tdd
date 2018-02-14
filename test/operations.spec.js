b292e07762d66427c956b357502e49eb53e59fc1
Nicole Bekkers <nicole@kabisa.nl>
2018-02-14 13:51:47 +0100

    REFACTOR: Rename file and description of tests
    These are operations of basic arithmetics, so let's reflect that in our code

--- test/operations.spec.js
+++ test/operations.spec.js
@@ -1,8 +1,8 @@
 import { expect } from "chai";
-import { add } from "../src/calculator";
+import { add } from "../src/operations";
 
-describe("Calculator", () => {
-  it("adds two numbers", () => {
+describe("Operations", () => {
+  it("adds two integers", () => {
     expect(add(1, 2)).to.eql(3)
     expect(add(2, 3)).to.eql(5)
   });
