b4c21384f41fbeed79ee35507ca60effc3e545e8
Nicole Bekkers <nicole@kabisa.nl>
2018-02-14 12:03:27 +0100

    RED: Similar test but expecting a different result

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -5,4 +5,8 @@ describe("Calculator", () => {
   it("adds 1 and 2 together", () => {
     expect(add(1, 2)).to.eql(3)
   });
+
+  it("adds 2 and 3 together", () => {
+    expect(add(2, 3)).to.eql(5)
+  });
 });
