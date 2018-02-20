ddf9d20705ec7a3cf08d71d44bcc792f6861dbf6
Nicole Bekkers <nicole@kabisa.nl>
2018-02-20 15:30:06 +0100

    REFACTOR: Remove duplication from the test

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -2,13 +2,17 @@ import { expect } from "chai";
 import { Calculator } from "../src/calculator";
 
 describe("Calculator", () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new Calculator()
+  });
+
   it("has a default result", () => {
-    const calculator = new Calculator()
     expect(calculator.result()).to.eql(0)
   });
 
   it("can add a number", () => {
-    const calculator = new Calculator()
     calculator.add(2)
     expect(calculator.result()).to.eql(2)
   });
