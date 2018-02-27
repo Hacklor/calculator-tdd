4cc232faa8b1899e731de082572cb9d3c3fb10b1
Nicole Bekkers <nicole@kabisa.nl>
2018-02-27 10:47:27 +0100

    REFACTOR: Divide tests blocks based on the function

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -12,37 +12,43 @@ describe("Calculator", () => {
     expect(calculator.result).to.eql(0)
   });
 
-  it("can add a number", () => {
-    calculator.add(2)
-    expect(calculator.result).to.eql(2)
+  describe("Addition", () => {
+    it("can add a number", () => {
+      calculator.add(2)
+      expect(calculator.result).to.eql(2)
+    });
+
+    it("can chain adding multiple numbers", () => {
+      calculator.add(1).add(3).add(5)
+      expect(calculator.result).to.eql(9)
+    });
   });
 
-  it("can chain adding multiple numbers", () => {
-    calculator.add(1).add(3).add(5)
-    expect(calculator.result).to.eql(9)
+  describe("Subtraction", () => {
+    it("can subtract a single number", () => {
+      calculator.subtract(3)
+      expect(calculator.result).to.eql(-3)
+    });
+
+    it("can chain subtracting multiple numbers", () => {
+      calculator.subtract(1).subtract(2).subtract(7)
+      expect(calculator.result).to.eql(-10)
+    });
   });
 
-  it("can subtract a single number", () => {
-    calculator.subtract(3)
-    expect(calculator.result).to.eql(-3)
-  });
+  describe("Multiplication", () => {
+    it("can multiply a single number", () => {
+      calculator.add(1)
 
-  it("can chain subtracting multiple numbers", () => {
-    calculator.subtract(1).subtract(2).subtract(7)
-    expect(calculator.result).to.eql(-10)
-  });
+      calculator.multiply(2)
+      expect(calculator.result).to.eql(2)
+    });
 
-  it("can multiply a single number", () => {
-    calculator.add(1)
+    it("can chain multipying multiple numbers", () => {
+      calculator.add(1)
 
-    calculator.multiply(2)
-    expect(calculator.result).to.eql(2)
-  });
-
-  it("can chain multipying multiple numbers", () => {
-    calculator.add(1)
-
-    calculator.multiply(3).multiply(5).multiply(2)
-    expect(calculator.result).to.eql(30)
+      calculator.multiply(3).multiply(5).multiply(2)
+      expect(calculator.result).to.eql(30)
+    });
   });
 });
