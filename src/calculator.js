38a6b543c92dffad94032b269c2bb49471a0ac6a
Nicole Bekkers <nicole@kabisa.nl>
2018-02-27 12:08:10 +0100

    REFACTOR: Remove semi-colons
    It's a habit that I keep putting them in, even though they are not strictly required anymore

--- src/calculator.js
+++ src/calculator.js
@@ -2,30 +2,30 @@ import { add, subtract, multiply, square } from "./operations";
 
 export class Calculator {
   constructor() {
-    this._result = 0;
+    this._result = 0
   }
 
   get result() {
-    return this._result;
+    return this._result
   }
 
   add(number) {
-    this._result = add(this._result, number);
-    return this;
+    this._result = add(this._result, number)
+    return this
   }
 
   subtract(number) {
-    this._result = subtract(this._result, number);
-    return this;
+    this._result = subtract(this._result, number)
+    return this
   }
 
   multiply(number) {
-    this._result = multiply(this._result, number);
-    return this;
+    this._result = multiply(this._result, number)
+    return this
   }
 
   square() {
     this._result = square(this._result)
-    return this;
+    return this
   }
 }
