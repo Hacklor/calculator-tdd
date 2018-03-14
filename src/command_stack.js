f98cd3839cc82fa89688eaf43dec6394db17724b
Nicole Bekkers <nicole@kabisa.nl>
2018-03-14 15:25:55 +0100

    GREEN: Delay the execution of adding a number
    We do this by adding it to the commands stack and wrapping the true call to the calculator in a
    lambda

--- src/command_stack.js
+++ src/command_stack.js
@@ -2,13 +2,16 @@ export class CommandStack {
 
   constructor(calculator) {
     this.calculator = calculator
+    this.commands = []
   }
 
   execute() {
+    this.commands.forEach( command => command() )
+
     return this.calculator.result
   }
 
   add(number) {
-    this.calculator.add(number)
+    this.commands.push( () => this.calculator.add(number) )
   }
 }
