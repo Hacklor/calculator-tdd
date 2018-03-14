eb5b3c6edc6e80d2f8fc452f32bc71b8eeeae629
Nicole Bekkers <nicole@kabisa.nl>
2018-03-14 13:55:15 +0100

    RED: Add new test that expects to delay a number
    The test name is not correct yet with the content of the test. But we are already in a red state, so
    let's fix it step by step

--- test/command_stack.spec.js
+++ test/command_stack.spec.js
@@ -1,10 +1,20 @@
 import { expect } from 'chai';
 import { CommandStack } from '../src/command_stack';
+import { Calculator } from '../src/calculator';
 
 describe("CommandStack", () => {
   it("executes and gets the default result from the calculator", () => {
     const stack = new CommandStack();
 
     expect(stack.execute()).to.eql(0)
+  });
+
+  it("delays adding a number", () => {
+    const calculator = new Calculator();
+    const stack = new CommandStack(calculator);
+
+    stack.add(4);
+
+    expect(stack.execute()).to.eql(4)
   });
 });
