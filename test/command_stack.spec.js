464d14f10650090174e8ecc4b65771f59231e78e
Nicole Bekkers <nicole@kabisa.nl>
2018-03-14 13:46:31 +0100

    REFACTOR: Remove first, now obsolete, test

--- test/command_stack.spec.js
+++ test/command_stack.spec.js
@@ -2,10 +2,6 @@ import { expect } from 'chai';
 import { CommandStack } from '../src/command_stack';
 
 describe("CommandStack", () => {
-  it("creates an instance", () => {
-    new CommandStack();
-  });
-
   it("executes and gets the default result from the calculator", () => {
     const stack = new CommandStack();
 
