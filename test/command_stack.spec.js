9b16697507dcaf9f3f88278dda5038747dee15d3
Nicole Bekkers <nicole@kabisa.nl>
2018-03-14 15:16:48 +0100

    RED: Add spy to test
    Sinon cannot be found, so we need to include that first

--- test/command_stack.spec.js
+++ test/command_stack.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { CommandStack } from '../src/command_stack';
 import { Calculator } from '../src/calculator';
+import sinon from 'sinon';
 
 describe("CommandStack", () => {
   let calculator, stack;
@@ -15,7 +16,10 @@ describe("CommandStack", () => {
   });
 
   it("delays adding a number", () => {
+    const spy = sinon.spy(calculator, 'add')
+
     stack.add(4)
+    expect(spy).not.to.be.called
 
     expect(stack.execute()).to.eql(4)
   });
