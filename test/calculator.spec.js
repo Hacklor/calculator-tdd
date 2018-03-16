7931e85fa4a1aba1546e68ec2ec581ef12bd5759
Nicole Bekkers <nicole@kabisa.nl>
2018-03-16 11:39:48 +0100

    REFACTOR: Fix the tests that had been broken
    Now that the chaining functionality has been removed we don't need the tests anymore that verify it
    for an operation. The regression test we need to change and add the calculator object

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -17,22 +17,12 @@ describe("Calculator", () => {
       calculator.add(2)
       expect(calculator.result).to.eql(2)
     });
-
-    it("can chain adding multiple numbers", () => {
-      calculator.add(1).add(3).add(5)
-      expect(calculator.result).to.eql(9)
-    });
   });
 
   describe("Subtraction", () => {
     it("can subtract a single number", () => {
       calculator.subtract(3)
       expect(calculator.result).to.eql(-3)
-    });
-
-    it("can chain subtracting multiple numbers", () => {
-      calculator.subtract(1).subtract(2).subtract(7)
-      expect(calculator.result).to.eql(-10)
     });
   });
 
@@ -45,11 +35,6 @@ describe("Calculator", () => {
       calculator.multiply(2)
       expect(calculator.result).to.eql(2)
     });
-
-    it("can chain multipying multiple numbers", () => {
-      calculator.multiply(3).multiply(5).multiply(2)
-      expect(calculator.result).to.eql(30)
-    });
   });
 
   describe("Square", () => {
@@ -59,22 +44,14 @@ describe("Calculator", () => {
       calculator.square()
       expect(calculator.result).to.eql(4)
     });
-
-    it("can chain squaring multiple numbers", () => {
-      calculator.add(3)
-
-      calculator.square().square()
-      expect(calculator.result).to.eql(81)
-    });
   });
 
   describe("Regression test", () => {
     it("can combine multiple chained commands", () => {
-      calculator
-        .add(7)
-        .subtract(2)
-        .square()
-        .multiply(4)
+      calculator.add(7)
+      calculator.subtract(2)
+      calculator.square()
+      calculator.multiply(4)
 
       expect(calculator.result).to.eql(100)
     });
