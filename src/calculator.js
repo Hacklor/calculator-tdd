0f92e754a2ac8c0e32e3857c745c0ae66494ffc7
Nicole Bekkers <nicole@kabisa.nl>
2018-03-16 11:36:49 +0100

    REFACTOR: Break the tests by removing the chaining code
    Normally you would not leave a build broken, but I commit it now to show that it can help detect
    where certain code is used. And where you need to go in order to fix it. A good test set can really
    guide you with that, without fear of breaking other functionality

--- src/calculator.js
+++ src/calculator.js
@@ -11,21 +11,17 @@ export class Calculator {
 
   add(number) {
     this._result = add(this._result, number)
-    return this
   }
 
   subtract(number) {
     this._result = subtract(this._result, number)
-    return this
   }
 
   multiply(number) {
     this._result = multiply(this._result, number)
-    return this
   }
 
   square() {
     this._result = square(this._result)
-    return this
   }
 }
